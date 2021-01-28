2e6422c94478cdc24196f495a8c668341fa1cad3
Kbob <anthony.capobianchi@gmail.com>
2021-01-28 11:44:15 -0500

    add heroku build config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = express();
 
 server.use( express.static( path.join( __dirname, 'webapp/build' ) ) );
 
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 server.listen( port );
 
 console.log( `Server listening on port ${port}` );
